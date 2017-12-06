c15f0a567f3291fc627f2a5bbd49a457aaea659e
Gabriel Albino <gabrielalbino@aluno.unb.br>
2017-12-05 23:16:57 -0200

    Adicionado para o usuário só visualizar as disciplinas que ele for o dono.

--- app/controllers/cadernos_controller.rb
+++ app/controllers/cadernos_controller.rb
@@ -1,6 +1,6 @@
 class CadernosController < ApplicationController
   before_action :set_caderno, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /cadernos
   # GET /cadernos.json
   def index
@@ -27,7 +27,7 @@ class CadernosController < ApplicationController
     @caderno = Caderno.new(caderno_params)
 
     respond_to do |format|
-      
+
       if @caderno.save
         format.html { redirect_to @caderno, notice: 'Caderno was successfully created.' }
         format.json { render :show, status: :created, location: @caderno }
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :disciplinas
   root 'disciplinas#index'
-  get 'welcome/index'
-
   resources :cadernos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
