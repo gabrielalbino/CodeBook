679d6d2c217faa3eaf1cd709ba9d863a77135882
Gabriel Albino <enggabrielalbino@gmail.com>
2017-12-08 11:57:49 -0300

    Adicionado avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-   	user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username)
+   	user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :username, :avatar)
   end
 end
 end
