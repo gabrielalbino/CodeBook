a2f6b9c4434d379fe4f2ad1a082ee6300770941d
Gabriel Albino <enggabrielalbino@gmail.com>
2017-12-09 17:48:42 -0300

    Adicionado pagina de visualizar cadernos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   match '/users',   to: 'users#index',   via: 'get'
   get '/pedidos/emprestar/:id', to: 'pedidos#emprestar', as: 'emprestar'
+  get '/pedidos/show', to: 'pedidos#show', as: 'emprestados'
   devise_for :users
   resources :pedidos
   resources :disciplinas
